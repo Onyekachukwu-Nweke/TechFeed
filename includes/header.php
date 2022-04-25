2d18549f52d2e5c48048e86c93dceeb990c05def
Onyekachukwu Ejiofor Nweke <nwekeejioforscheller@gmail.com>
2022-04-25 05:26:10 -0700

    Styling User details

--- includes/header.php
+++ includes/header.php
@@ -50,3 +50,5 @@ else {
 
 
     </div>
+
+    <div class="wrapper">
--- index.php
+++ index.php
@@ -1,6 +1,27 @@
 <?php 
     include("includes/header.php");
 ?>
-    <?php echo "Hello " . $userLoggedIn . "!!" ?>
+    <div class="user_details column">
+        <a href="#"><img src="<?php echo $user['profile_pic']; ?>" alt=""></a>
+
+        <div class="user_details_left_right">
+            <a href="#">
+                <?php
+                    echo $user['first_name'] . " " . $user['last_name'] . "<br>";
+
+                ?>
+            </a>
+
+            <?php 
+                echo "Posts:" . $user['num_posts'] . "<br>";
+                echo "Likes:" . $user['num_likes'];
+            ?>
+        </div>
+        
+
+    </div>
+
+
+    </div>
 </body>
 </html>
